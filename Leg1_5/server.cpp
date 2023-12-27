b55965eaf26af74e1ed50afbe476e8dbcddf0813
jiao <dxlcq@outlook.com>
2023-12-27 21:11:32 +0800

    add http server pro

---
+++ Leg1_5/server.cpp
@@ -0,0 +1,81 @@
+#include "tool.h"
+
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <string>
+
+int main(int argc, char* args[]){
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    err(server == -1, "socket创建错误");
+
+    sockaddr_in server_addr;
+    memset(&server_addr, '\0', sizeof server_addr);
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port        = htons(std::stoi(args[1]));
+
+    err(
+        bind(server, (sockaddr*)&server_addr, sizeof server_addr) == -1,
+        "绑定服务端地址错误"
+    );
+
+    err(
+        listen(server, 5) == -1,
+        "服务端监听错误"
+    );
+
+    while(true){
+        // 接受客户端连接
+        sockaddr_in client_addr;
+        memset(&client_addr, '\0', sizeof client_addr);
+        socklen_t   client_addr_len = sizeof client_addr;
+        int client = accept(server, (sockaddr*)&server_addr, &client_addr_len);
+        if( war(client==-1, "接受客户端连接错误") )
+            continue;
+        else
+            std::cout << "\n新的连接：" << client << " IP地址：" << inet_ntoa(client_addr.sin_addr) << std::endl;
+
+        char buf[10240];
+        while(true){ 
+            // 接收数据
+            memset(buf, '\0', sizeof buf);
+            std::cout << "等待接受数据" << client << std::endl;      
+            int len = recv(client, buf, sizeof buf, 0);
+            if( war(len <= 0, "接收数据错误") ){
+                close(client);
+                break;
+            } else 
+                std::cout << "\n接收数据成功，长度：" << len << "；内容：\n" << buf << std::endl;
+
+            // 发送数据
+            memset(buf, '\0', sizeof buf);
+            // 1. 发送状态行
+            std::string content = "HTTP/1.1 200 OK\r\n";
+            // 2. 发送响应头
+            content += "Connection: keep-alive\r\n";
+            // 3. 发送空行
+            content += "\r\n";
+            // 4. 发送响应体
+            // 读取文件
+            std::ifstream file("index.html");
+            std::string   line;
+            while( std::getline(file, line) )
+                content += line;
+            // 发送
+            if( war(send(client, content.c_str(), content.size(), 0) <= 0, "发送数据错误") )
+                break;
+            else
+                std::cout << "发送数据成功！\n" << std::endl;
+
+            close(client);
+            break;
+        }
+    }
+
+    close(server);
+    return 0;
+}
